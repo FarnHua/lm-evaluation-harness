4ce3e0e0b8c9df0f9ea54771cc332eb222fa4743
farnhua <alhena.farn@gmail.com>
2023-09-02 00:31:07 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export TRANSFORMERS_CACHE=/work/u5273929/huggingface_hub
-export HF_DATASETS_CACHE=/work/u5273929/huggingface_hub
-export HUGGINGFACE_HUB_CACHE=/work/u5273929/huggingface_hub
-
 
 TASK=$1
 MODEL_ARGS=$2
@@ -14,24 +10,12 @@ mkdir -p $OUTPUT_PATH
 
 python main.py \
     --model hf-causal-experimental \
-    --model_args MODEL_ARGS \
+    --model_args $MODEL_ARGS \
     --tasks $TASK  \
     --num_fewshot $FEW_SHOT \
     --batch_size 2 \
     --output_path $OUTPUT_PATH/result.json \
     --prompts_file $PROMPTS_FILE \
     --no_cache --use_prompt &> $OUTPUT_PATH/eval.log
-
-
-# python main.py \
-#     --model hf-causal-experimental \
-#     --model_args pretrained=meta-llama/Llama-2-13b-chat-hf,use_accelerate=True \
-#     --tasks arc_challenge  \
-#     --num_fewshot 0 \
-#     --batch_size 2 \
-#     --output_path results/test.json \
-#     --no_cache \
-#     --limit 2 \
-#     --prompts_file prompt.json
 
 
