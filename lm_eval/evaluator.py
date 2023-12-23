db4e981178304214cdbb891bca12c25d7de187f6
farnhua <alhena.farn@gmail.com>
2023-12-24 00:17:50 +0800

    update template

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -157,9 +157,9 @@ def get_prompt(sentence, system_prompt='', user_prompt=''):
         # system_prompt = "You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.\n\nIf a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."
         system_prompt = f'<<SYS>>\n{system_prompt}\n<</SYS>>\n\n'
         texts = [f'[INST] {system_prompt}']
-        texts.append(f"{user_prompt}{sentence} [/INST]")
+        texts.append(f"{user_prompt} {sentence} [/INST]")
     else:
-        texts.append(f"{user_prompt}{sentence}")
+        texts.append(f"[INST] {user_prompt} {sentence} [/INST]")
 
     return ''.join(texts)
 
